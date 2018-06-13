e5f11087c47f7d30999df2c511f2b4e512642d42
hawknewton <hawk.newton@gmail.com>
2018-06-13 09:56:56 -0700

    List dotfiles

--- lib/gitignore/parser.rb
+++ lib/gitignore/parser.rb
@@ -5,7 +5,7 @@ require 'gitignore/parser/rule'
 module Gitignore; end
 
 module Gitignore::Parser
-  def self.list_files(opts)
+  def self.list_files(opts = {})
     Gitignore::Parser::Scanner.new(opts).list_files
   end
 end
--- spec/gitignore/parser/scanner_spec.rb
+++ spec/gitignore/parser/scanner_spec.rb
@@ -18,6 +18,7 @@ RSpec.describe Gitignore::Parser::Scanner do
     let(:directory) { 'spec/data/one_gitignore' }
     it 'ignores the correct files' do
       expect(result).to match_array([
+        'spec/data/one_gitignore/.gitignore',
         'spec/data/one_gitignore/keep1',
         'spec/data/one_gitignore/keep2',
         'spec/data/one_gitignore/keep/keep_me',
@@ -34,7 +35,9 @@ RSpec.describe Gitignore::Parser::Scanner do
     let(:directory) { 'spec/data/two_gitignores' }
     it 'ignores the correct files' do
       expect(result).to match_array([
+        'spec/data/two_gitignores/.gitignore',
         'spec/data/two_gitignores/keep',
+        'spec/data/two_gitignores/subdir/.gitignore',
         'spec/data/two_gitignores/subdir/keep_too'
       ])
     end
@@ -44,8 +47,10 @@ RSpec.describe Gitignore::Parser::Scanner do
     let(:directory) { 'spec/data/ebignore' }
     it 'returns all files' do
       expect(result).to match_array([
+        'spec/data/ebignore/.ebignore',
         'spec/data/ebignore/drop',
         'spec/data/ebignore/keep',
+        'spec/data/ebignore/subdir/.ebignore',
         'spec/data/ebignore/subdir/drop',
         'spec/data/ebignore/subdir/drop_too',
         'spec/data/ebignore/subdir/keep_too'
@@ -56,7 +61,9 @@ RSpec.describe Gitignore::Parser::Scanner do
       let(:opts) { { filename: '.ebignore' } }
       it 'ignores the correct files' do
         expect(result).to match_array([
+          'spec/data/ebignore/.ebignore',
           'spec/data/ebignore/keep',
+          'spec/data/ebignore/subdir/.ebignore',
           'spec/data/ebignore/subdir/keep_too'
         ])
       end
@@ -67,10 +74,25 @@ RSpec.describe Gitignore::Parser::Scanner do
     let(:directory) { 'spec/data/unignore' }
     it 'returns unignored files' do
       expect(result).to match_array([
+        'spec/data/unignore/.gitignore',
         'spec/data/unignore/keep',
         'spec/data/unignore/keep.txt',
+        'spec/data/unignore/subdir/.gitignore',
         'spec/data/unignore/subdir/keep',
         'spec/data/unignore/subdir/keep_too'
+      ])
+    end
+  end
+
+  context 'given a directory with dotfiles' do
+    let(:directory) { 'spec/data/dotfiles' }
+    it 'returns dotfiles' do
+      expect(result).to match_array([
+        'spec/data/dotfiles/.gitignore',
+        'spec/data/dotfiles/.keep',
+        'spec/data/dotfiles/keep_too',
+        'spec/data/dotfiles/.keep_dir/keep_three',
+        'spec/data/dotfiles/.keep_dir/.keep_again/keep_four',
       ])
     end
   end

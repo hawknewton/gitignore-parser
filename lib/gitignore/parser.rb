06c7d62329467d38816a9f5a53c299bae1aca0b0
hawknewton <hawk.newton@gmail.com>
2018-06-13 09:16:37 -0700

    Ensure Gitignore module exists

--- lib/gitignore/parser.rb
+++ lib/gitignore/parser.rb
@@ -2,6 +2,8 @@ require 'gitignore/parser/scanner'
 require 'gitignore/parser/version'
 require 'gitignore/parser/rule'
 
+module Gitignore; end
+
 module Gitignore::Parser
   def self.list_files(opts)
     Gitignore::Parser::Scanner.new(opts).list_files
